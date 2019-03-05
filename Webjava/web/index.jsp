da5be752ce1d160a770f429c4545682dd24b1b2b
wuzhaowen <513956702@qq.com>
2019-03-05 20:27:12 +0800

    Initial commit

---
+++ Webjava/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: bigbug
+  Date: 2019/3/5
+  Time: 19:43
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>shangguiguguanwnag</title>
+  </head>
+  <body>
+   <h1 style="color: olivedrab;">hello ssssssss</h1>
+  </body>
+</html>
---
+++ untitled1/src/atguigu/com/java/Hello.java
@@ -0,0 +1,23 @@
+package atguigu.com.java;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.printf("____________________");
+
+        System.out.println("args = [" + args + "]");
+        int a = 0;
+        System.out.println("a = " + a);
+        String  b = "123";
+        System.out.println(b);
+        for (int i = 0; i <10; i++) {
+
+            if (b == null) {
+
+            }
+
+        }
+
+    }
+}
